86c667e3e3ba6b53b5b0388f2268a5126e942026
rasyidzkun <rasyidzkun@gmail.com>
2022-06-26 18:31:43 +0700

    fix bug

--- src/sorting/bubble_sort.java
+++ src/sorting/bubble_sort.java
@@ -1,7 +1,7 @@
 public class bubble_sort {
     static void bubbleSort(int[] arr) {
         for(int i = 0; i < arr.length; i++) {
-            for(int j = 0 + 1; j < arr.length - 1; j++) {
+            for(int j = 0; j < arr.length - 1; j++) {
                 if(arr[j + 1] < arr[j]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
